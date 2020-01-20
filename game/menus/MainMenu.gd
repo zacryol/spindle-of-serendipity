0b52d4f0db4a8147988ef9b1dfdf6bd97c00be67
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-20 11:38:43 -0700

    Import Json functionality

--- game/menus/MainMenu.gd
+++ game/menus/MainMenu.gd
@@ -8,3 +8,11 @@ func _on_ImportButton_pressed():
 	$FileDialog.current_path = "res://"
 	$FileDialog.show()
 	pass # Replace with function body.
+
+func _on_FileDialog_files_selected(paths):
+	for path in paths:
+		var file_name = Array((path as String).split("/")).back()
+		EntryImport.import_entries_from_file(path)
+		Directory.new().copy(path, GlobalVars.ENTRIES_SAVE + file_name)
+	
+	pass # Replace with function body.
