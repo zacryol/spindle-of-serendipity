d1fe8f2e3a2ae9bd9c6a676e52afaafb89e87b0a
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-20 08:57:14 -0700

    Set Initial Functions for Entry script

--- system/Entry.gd
+++ system/Entry.gd
@@ -1,16 +1,19 @@
 extends Object
 
+var text : String
+var category : String
+var source : String
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _init(new_text : String, new_category := "", new_source := ""):
+	text = new_text
+	category = new_category
+	source = new_source
 
+func get_entry_text() -> String:
+	return text
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_import_category() -> String:
+	return category
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_import_source() -> String:
+	return source
