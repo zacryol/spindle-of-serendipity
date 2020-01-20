e8a30c8b3f1655da73ac1cdf7dc4e14f4fbbfd82
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-20 09:02:29 -0700

    Added GlobalVars singleton

--- system/Entry.gd
+++ system/Entry.gd
@@ -4,7 +4,7 @@ var text : String
 var category : String
 var source : String
 
-func _init(new_text : String, new_category := "", new_source := ""):
+func _init(new_text : String, new_category := GlobalVars.DEFAULT_CATEGORY, new_source := GlobalVars.DEFAULT_SOURCE):
 	text = new_text
 	category = new_category
 	source = new_source
