a792f99263dd4459fe1df9ef2355d006570fff4f
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-20 11:16:33 -0700

    Set Main Menu as main scene

--- game/menus/MainMenu.gd
+++ game/menus/MainMenu.gd
@@ -4,5 +4,7 @@ func _ready():
 	EntryImport.grab_saved_files()
 
 func _on_ImportButton_pressed():
+	$FileDialog.current_dir = "res://"
+	$FileDialog.current_path = "res://"
 	$FileDialog.show()
 	pass # Replace with function body.
