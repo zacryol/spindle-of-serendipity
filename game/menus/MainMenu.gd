bbb178d0709a9b7e26efd57bf815083fd02884ca
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-20 11:44:51 -0700

    Restructued File read method

--- game/menus/MainMenu.gd
+++ game/menus/MainMenu.gd
@@ -11,8 +11,12 @@ func _on_ImportButton_pressed():
 
 func _on_FileDialog_files_selected(paths):
 	for path in paths:
-		var file_name = Array((path as String).split("/")).back()
 		EntryImport.import_entries_from_file(path)
-		Directory.new().copy(path, GlobalVars.ENTRIES_SAVE + file_name)
+		
+		var file_name = Array(path.split("/")).back()
+		var save_path = GlobalVars.ENTRIES_SAVE + file_name
+		if File.new().file_exists(save_path):
+			pass
+		Directory.new().copy(path, save_path)
 	
 	pass # Replace with function body.
