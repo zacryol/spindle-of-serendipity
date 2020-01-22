f87f7afdfd6743607c7700159f8449096f7491b0
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-21 18:45:40 -0700

    Cleared Testing scene

--- testing/TestMenu.gd
+++ testing/TestMenu.gd
@@ -1,6 +1,4 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	var e = Entry.new("Text", "Category")
-	pass # Replace with function body.
+	pass
