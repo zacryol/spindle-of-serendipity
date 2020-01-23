84c817d1204695273cc4c672d4e5f47a2f265fe8
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-22 17:53:37 -0700

    Fixed typing issue

--- game/components/Keyboard.gd
+++ game/components/Keyboard.gd
@@ -6,9 +6,8 @@ func _ready():
 	var key_container := $CenterContainer/GridContainer
 	var keys := key_container.get_children()
 	for key in keys:
-		(key as Button).connect("pressed", self, "_on_Key_pressed", [key.text])
-		pass
-	pass
+		if key is Button:
+			key.connect("pressed", self, "_on_Key_pressed", [key.text])
 
 func _on_Key_pressed(letter : String):
 	emit_signal("key_pressed", letter)
