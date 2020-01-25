f3523beb35f6107dfbb85e87b8ee6401d90d1657
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-25 07:48:19 -0700

    Created basic turn queue

--- game/components/PlayersPanel.gd
+++ game/components/PlayersPanel.gd
@@ -6,12 +6,12 @@ onready var p3 := $PanelContainer/VBoxContainer/Player3
 onready var players_array = [p1, p2, p3]
 var current_player = 0
 
-# Maximum INDEX, not quantity
-const MAX_PLAYER = 2
+const NUM_PLAYER = 3
 
 func _ready():
 	pass
 
 func _letters_guessed(number: int):
 	players_array[current_player].add_to_score(number)
+	current_player = wrapi(current_player + 1, 0, NUM_PLAYER)
 	pass
