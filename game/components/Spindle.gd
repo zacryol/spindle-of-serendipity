c72be0a03c1783c9c858ed74a5459b1929f349fb
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-26 16:04:44 -0700

    Can't spam spindle

--- game/components/Spindle.gd
+++ game/components/Spindle.gd
@@ -2,13 +2,17 @@ extends Control
 
 var current_value : int
 signal score(points)
+var enabled := true
 
 func _ready():
 	pass
 
 func _letters_guessed(number: int):
 	emit_signal("score", number * current_value)
+	enabled = true
 
 func _on_Button_pressed():
-	current_value = (randi() % 10 + 1) * 5
-	$ScoringLabel.text = "Score: " + str(current_value)
+	if enabled:
+		current_value = (randi() % 10 + 1) * 5
+		$ScoringLabel.text = "Score: " + str(current_value)
+		enabled = false
