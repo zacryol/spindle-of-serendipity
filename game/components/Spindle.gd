14c47a706dc5364db43b5807eb49bbed32579e59
Zach <60046681+zacryol@users.noreply.github.com>
2020-01-26 15:39:21 -0700

    Added signal functions to Spindle script

--- game/components/Spindle.gd
+++ game/components/Spindle.gd
@@ -1,10 +1,13 @@
 extends Control
 
 var current_value : int
+signal score(points)
 
 func _ready():
 	pass
 
+func _letters_guessed(number: int):
+	emit_signal("score", number * current_value)
 
 func _on_Button_pressed():
 	current_value = (randi() % 10 + 1) * 5
