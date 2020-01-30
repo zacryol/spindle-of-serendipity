6700b2aa1276fec519c2677e2ff810003dff7a12
zacryol <60046681+zacryol@users.noreply.github.com>
2020-01-29 20:39:23 -0700

    Tweaked displays

--- game/GamePanel.gd
+++ game/GamePanel.gd
@@ -2,11 +2,15 @@ extends Control
 
 onready var entry_display := $PanelContainer/VBoxContainer/PanelContainer2/HSplitContainer/VSplitContainer/EntryDisplay
 onready var log_label := $PanelContainer/VBoxContainer/PanelContainer/Label
+const MAX_LOG_CHAR := 150
 
 func _ready():
 	entry_display.set_display(EntryManager.get_random_entry())
-	
+	log_label.text = ""
 	pass
 
 func _log(text: String):
-	log_label.text = text
+	var new_text := str(log_label.text) + text + " -- "
+	while new_text.length() > MAX_LOG_CHAR:
+		new_text = new_text.substr(1)
+	log_label.text = new_text
