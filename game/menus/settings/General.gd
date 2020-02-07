5a0f8f9d84560def5850735bfdbf9c25cf953bbd
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-02-07 13:54:22 -0700

    Connected OptionButton signals

--- game/menus/settings/General.gd
+++ game/menus/settings/General.gd
@@ -1,5 +1,18 @@
 extends Tabs
 
+onready var source_options := $Scroll/List/RevealSource/OptionButton
+onready var rand_options := $Scroll/List/RandMode/OptionButton
+
 
 func _ready():
+	source_options.select(GlobalVars.show_source)
+	rand_options.select(GlobalVars.rand_mode)
 	pass
+
+
+func _on_source_option_selected(id):
+	pass # Replace with function body.
+
+
+func _on_rand_option_selected(id):
+	pass # Replace with function body.
