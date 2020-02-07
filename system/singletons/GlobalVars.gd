fb7d2edf9ae05b252915b03dd117ccd36daf932d
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-02-07 10:54:38 -0700

    Created base for settings menu

--- system/singletons/GlobalVars.gd
+++ system/singletons/GlobalVars.gd
@@ -4,6 +4,24 @@ const DEFAULT_CATEGORY := "Miscellaneous"
 const DEFAULT_SOURCE := "N/A"
 
 const ENTRIES_SAVE := "user://entries/"
+const SETTINGS_SAVE := "user://settings/general"
+
+# Settings constants and vars
+enum {
+	SOURCE_NEVER
+	SOURCE_SOLVE
+	SOURCE_ALWAYS
+}
+var show_source := SOURCE_SOLVE
+
+enum {
+	RAND_NON
+	RAND_CAT
+	RAND_SOU
+}
+var rand_mode := RAND_NON
+
+# End of Settings
 
 func _ready():
 	randomize()
