80ac52a33782e4e25bb1dd1672a3a1ec0ef69a74
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-02-07 18:49:26 -0700

    Setting up sub tabs for aliases

--- game/menus/settings/AliasMenu.gd
+++ game/menus/settings/AliasMenu.gd
@@ -1,5 +1,7 @@
 extends Tabs
 
+onready var cat := $T/Categories
+onready var sou := $T/Sources
 
 func _ready():
 	pass
---
+++ game/menus/settings/AliasSubTab.gd
@@ -0,0 +1,10 @@
+extends Tabs
+
+onready var imports := $H/Import/VBox
+onready var outs := $H/View/VBox
+
+func _ready():
+	pass
+
+func set_ui(import: PoolStringArray):
+	pass
