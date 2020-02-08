f285aea7f192443d132d751565870e1d1f2f1ee4
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-02-07 18:35:09 -0700

    Fixed save/load functions to separate category and source

--- system/singletons/Alias.gd
+++ system/singletons/Alias.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var aliases : Dictionary
+var aliases_cat : Dictionary
+var aliases_sou : Dictionary
 
 func _ready():
 	pass
@@ -8,19 +9,27 @@ func _ready():
 
 func load_from_file():
 	var f := File.new()
-	if not f.file_exists(GlobalVars.ALIAS_SAVE):
-		return
-	f.open(GlobalVars.ALIAS_SAVE, File.READ)
-	var s := f.get_as_text()
-	var v := validate_json(s)
-	if not v:
-		var a = parse_json(s)
-		if typeof(a) == TYPE_DICTIONARY:
-			aliases = a
+	if f.file_exists(GlobalVars.ALIAS_SAVE):
+		f.open(GlobalVars.ALIAS_SAVE, File.WRITE)
+		var s := f.get_as_text()
+		var v = validate_json(s)
+		if not v:
+			var a = parse_json(s)
+			if typeof(a) == TYPE_DICTIONARY:
+				var d := a as Dictionary
+				if d.has("cat"):
+					aliases_cat = d["cat"]
+				if d.has("sou"):
+					aliases_sou = d["sou"]
+	f.close()
 
 
 func save_to_file():
+	var save_dict := {
+		"cat" : aliases_cat,
+		"sou" : aliases_sou
+	}
 	var f := File.new()
 	f.open(GlobalVars.ALIAS_SAVE, File.WRITE)
-	if not aliases.empty():
-		f.store_string(to_json(aliases))
+	f.store_string(to_json(save_dict))
+	f.close()
