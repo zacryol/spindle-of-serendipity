d4f5e91de7504a97d91daea4c8186be2c3b81e0d
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-02-07 18:14:38 -0700

    Wrote save and load functions

--- system/singletons/Alias.gd
+++ system/singletons/Alias.gd
@@ -7,8 +7,20 @@ func _ready():
 
 
 func load_from_file():
-	pass
+	var f := File.new()
+	if not f.file_exists(GlobalVars.ALIAS_SAVE):
+		return
+	f.open(GlobalVars.ALIAS_SAVE, File.READ)
+	var s := f.get_as_text()
+	var v := validate_json(s)
+	if not v:
+		var a = parse_json(s)
+		if typeof(a) == TYPE_DICTIONARY:
+			aliases = a
 
 
 func save_to_file():
-	pass
+	var f := File.new()
+	f.open(GlobalVars.ALIAS_SAVE, File.WRITE)
+	if not aliases.empty():
+		f.store_string(to_json(aliases))
