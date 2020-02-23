eb17646717b30ea1b8f948d7d3e677668e4e6771
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-02-23 16:13:25 -0700

    Quit button takes you to main

--- game/GamePanel.gd
+++ game/GamePanel.gd
@@ -3,6 +3,7 @@ extends Control
 onready var entry_display := $GP/VBoxContainer/Game/HSplit/EDKB/EntryDisplay
 onready var log_label := $GP/VBoxContainer/Top/HB/Label
 onready var new_button := $GP/VBoxContainer/Top/HB/NewG
+onready var quit_button := $GP/VBoxContainer/Top/HB/Quit
 onready var spindle := $GP/VBoxContainer/Game/HSplit/SP/Spindle
 onready var players := $GP/VBoxContainer/Game/HSplit/SP/PlayersPanel
 const MAX_LOG_CHAR := 150
@@ -27,8 +28,14 @@ func _on_NewG_pressed():
 	entry_display.set_display(EntryManager.get_random_entry())
 	log_label.text = ""
 	new_button.hide()
+	quit_button.hide()
 	spindle.enabled = true
 
 
 func _pre_reset():
 	new_button.show()
+	quit_button.show()
+
+
+func _on_Quit_pressed():
+	get_tree().change_scene("res://game/menus/MainMenu.tscn")
