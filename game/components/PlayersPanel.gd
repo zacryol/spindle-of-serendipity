14fe4f84add0d1d27e1e71d9e587ec313b347cd9
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-01 15:09:41 -0700

    Player advances on solve

--- game/components/PlayersPanel.gd
+++ game/components/PlayersPanel.gd
@@ -18,8 +18,8 @@ func _ready():
 func _score_gained(number: int, final: bool):
 	current_player().add_to_score(number)
 	emit_signal("game_log", str(number) + " points gained")
+	current_player = wrapi(current_player + 1, 0, NUM_PLAYER)
 	if not final:
-		current_player = wrapi(current_player + 1, 0, NUM_PLAYER)
 		emit_signal("game_log", current_player().player_name + " spin!")
 	else:
 		emit_signal("game_log", "You solved it!")
