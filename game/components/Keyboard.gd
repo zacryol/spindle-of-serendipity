5351b7d4fc75f35ede32954288990ed8248cc1c1
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-02 17:52:52 -0700

    Function to get keyboard key by text

--- game/components/Keyboard.gd
+++ game/components/Keyboard.gd
@@ -1,9 +1,9 @@
 extends "res://game/components/GameComponent.gd"
 
+onready var key_container := $CenterContainer/GridContainer
 signal key_pressed(letter)
 
 func _ready():
-	var key_container := $CenterContainer/GridContainer
 	var keys := key_container.get_children()
 	for key in keys:
 		if key is Button:
@@ -21,3 +21,11 @@ func _input(event):
 			&& event.is_pressed():
 		var e := str(event.as_text().right(event.as_text().length() - 1))
 		emit_signal("key_pressed", e)
+
+
+func get_button(letter : String) -> Button:
+	for key in key_container:
+		if key is Button:
+			if key.text == letter:
+				return key
+	return null
