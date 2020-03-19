c9105e1a9b92b6bb41306576145ec9a83c176594
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-19 11:15:57 -0600

    Expanded script

--- deploy-game.sh
+++ deploy-game.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo Removing old builds . . . 
 rm export/lin/*
+rm export/win/*
+echo Old builds removed . . . 
+
+echo Exporting Linux build . . . 
 godot-3.2.1-headless --export "Linux/X11" export/lin/spindle-of-serendipity.run
 
+echo Exporting Windows build
+godot-3.2.1-headless --export "Windows Desktop" export/win/spindle-of-serendipity.exe
+
+echo Done!
