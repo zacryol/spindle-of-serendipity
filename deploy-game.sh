1fe2fc44597697389a17402348522b0bcb65501f
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-19 11:18:41 -0600

    Added Mac build

--- deploy-game.sh
+++ deploy-game.sh
@@ -3,6 +3,7 @@
 echo Removing old builds . . . 
 rm export/lin/*
 rm export/win/*
+rm export/mac/*
 echo Old builds removed . . . 
 
 echo Exporting Linux build . . . 
@@ -10,5 +11,8 @@ godot-3.2.1-headless --export "Linux/X11" export/lin/spindle-of-serendipity.run
 
 echo Exporting Windows build
 godot-3.2.1-headless --export "Windows Desktop" export/win/spindle-of-serendipity.exe
+
+echo Exporting Mac build
+godot-3.2.1-headless --export "Mac OSX" export/mac/spindle-of-serendipity.zip
 
 echo Done!
