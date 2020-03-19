f5141baec3ba910fb08eea4f3897f80e6cdb7d50
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-19 11:49:52 -0600

    uses variable now

--- bump.sh
+++ bump.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-
+VERSION_FILE=version.txt
 if [ $# -eq 0 ]
 then
-	VERSION=$(cat version.txt )
+	VERSION=$(cat $VERSION_FILE)
 else
 	VERSION=$1
-	echo $VERSION > version.txt
+	echo $VERSION > $VERSION_FILE
 fi
 
 echo $VERSION
