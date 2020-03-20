12223b36878e73f3a531325ec4f95ab1c0f899ea
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-20 10:16:25 -0600

    Use variable before append

--- system/singletons/EntryManager.gd
+++ system/singletons/EntryManager.gd
@@ -10,12 +10,14 @@ func _to_string() -> String:
 
 
 func add_entry(data: PoolStringArray):
+	var e: Entry
 	if data.size() == 1:
-		entries.append(Entry.new(data[0]))
+		e = Entry.new(data[0])
 	elif data.size() == 2:
-		entries.append(Entry.new(data[0], data[1]))
+		e = Entry.new(data[0], data[1])
 	elif data.size() >= 3:
-		entries.append(Entry.new(data[0], data[1], data[2]))
+		e = Entry.new(data[0], data[1], data[2])
+	entries.append(e)
 
 
 func reset_picked() -> void:
