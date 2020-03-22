7753a98e894fdbaabfce4fdaacff7db37b3a3168
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-22 16:24:17 -0600

    Profiles Tab collects imported categories and sources

--- game/menus/settings/ProfilesTab.gd
+++ game/menus/settings/ProfilesTab.gd
@@ -1,5 +1,16 @@
-extends Control
+extends ScrollContainer
 
+onready var categories := $VBoxContainer/Config/Categories/List
+onready var sources := $VBoxContainer/Config/Sources/List
 
 func _ready():
+	for cat in EntryManager.get_import_categories():
+		var l := Label.new()
+		l.text = cat
+		categories.add_child(l)
+	
+	for sou in EntryManager.get_import_sources():
+		var l := Label.new()
+		l.text = sou
+		sources.add_child(l)
 	pass
