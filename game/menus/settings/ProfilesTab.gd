5d402627af4a60654b746bd10120d564395078bf
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-22 16:28:43 -0600

    Shows Alias info

--- game/menus/settings/ProfilesTab.gd
+++ game/menus/settings/ProfilesTab.gd
@@ -7,10 +7,14 @@ func _ready():
 	for cat in EntryManager.get_import_categories():
 		var l := Label.new()
 		l.text = cat
+		if Alias.has(cat, true):
+			l.text += " -> " + Alias.category(cat).to_lower()
 		categories.add_child(l)
 	
 	for sou in EntryManager.get_import_sources():
 		var l := Label.new()
 		l.text = sou
+		if Alias.has(sou, false):
+			l.text += " -> " + Alias.source(sou).to_lower()
 		sources.add_child(l)
 	pass
