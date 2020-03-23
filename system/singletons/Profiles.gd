749097c0ddf2820ff2507b1d364097501af93793
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-23 13:11:09 -0600

    Added return types

--- system/singletons/Profiles.gd
+++ system/singletons/Profiles.gd
@@ -2,12 +2,12 @@ extends Node
 
 var profiles_dict: Dictionary # string for key, Profile object as value
 
-func _ready():
+func _ready() -> void:
 	pass
 
 
 func add_profile(id: String,
-		cat: PoolStringArray, sou: PoolStringArray, both: bool):
+		cat: PoolStringArray, sou: PoolStringArray, both: bool) -> void:
 	profiles_dict[id] = Profile.new(cat, sou, both)
 	pass
 
@@ -17,15 +17,15 @@ class Profile extends Object:
 	var categories: PoolStringArray
 	var sources: PoolStringArray
 	
-	func _init(cat: PoolStringArray, sou: PoolStringArray, both: bool):
+	func _init(cat: PoolStringArray, sou: PoolStringArray, both: bool) -> void:
 		categories = cat
 		sources = sou
 		match_both = both
 	
 	
-	func append_category(cat: String):
+	func append_category(cat: String) -> void:
 		categories.append(cat)
 	
 	
-	func append_source(sou: String):
+	func append_source(sou: String) -> void:
 		sources.append(sou)
