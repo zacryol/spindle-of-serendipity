4e727dd7666e72c25d73c3235564bf046e1fdc76
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-25 13:05:16 -0600

    Removed commented code

--- game/GameSetting.gd
+++ game/GameSetting.gd
@@ -33,9 +33,6 @@ func _on_Button_pressed():
 	if GlobalVars.p3_name.length() > GlobalVars.PLAYER_NAME_MAX:
 		GlobalVars.p3_name = GlobalVars.p3_name.substr(0, GlobalVars.PLAYER_NAME_MAX)
 	
-#	print(EntryManager.is_profile_valid(
-#			Profiles.profiles_dict[selector.get_item_text(
-#			selector.get_selected_id())]))
 	var pid: String = selector.get_item_text(selector.get_selected_id())
 	
 	if selector.get_selected_id() == 0:
