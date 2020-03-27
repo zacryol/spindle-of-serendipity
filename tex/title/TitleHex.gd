e712647dae17e21df12443bbbf9ca204f72443b8
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-27 11:18:41 -0600

    Text sets properly

--- tex/title/TitleHex.gd
+++ tex/title/TitleHex.gd
@@ -4,8 +4,9 @@ extends TextureRect
 export var text: String setget set_text
 
 func _ready():
-	pass
+	$Label.text = text
 
 
 func set_text(tex: String):
 	$Label.text = tex.substr(0, 1)
+	text = tex
