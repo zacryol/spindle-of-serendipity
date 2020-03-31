f38094aa51efc0b781034144c7170a13f6352f0e
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-31 12:03:52 -0600

    Added text var and setget

--- godot/hex_panel/HexNode.gd
+++ godot/hex_panel/HexNode.gd
@@ -11,7 +11,15 @@ const COLOR_BLOCKED = Color.black
 const COLOR_TEMP = Color.red
 const COLOR_NONE = Color.white
 
-var core_text: String = ""
+var core_text: String = "" setget set_core, get_core
 
 func _ready():
 	pass
+
+
+func set_core(new_core: String):
+	core_text = new_core.substr(0, 1)
+
+
+func get_core() -> String:
+	return core_text
