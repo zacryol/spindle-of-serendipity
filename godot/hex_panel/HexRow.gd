c6e0cff8f11aa39250e3ad690a6775a0181186a7
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-31 12:39:50 -0600

    Added HexRow script

---
+++ godot/hex_panel/HexRow.gd
@@ -0,0 +1,16 @@
+extends HBoxContainer
+
+export var line_text: String setget set_line, get_line
+
+var HexNode: PackedScene = preload("res://hex_panel/HexNode.tscn")
+
+func _ready():
+	pass
+
+
+func set_line(text: String):
+	pass
+
+
+func get_line() -> String:
+	return line_text
