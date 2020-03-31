fa9da28aff482fe079a8c7a330bc17bb818e6da0
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-31 11:34:24 -0600

    Added more values

--- godot/hex_panel/HexNode.gd
+++ godot/hex_panel/HexNode.gd
@@ -1,8 +1,15 @@
 extends TextureRect
 
+enum State {
+	BLOCKED,
+	REVEALED,
+	TEMP,
+}
+
 const COLOR_REVEALED = Color(0.32, 0.65, 1.00, 1.00)
 const COLOR_BLOCKED = Color.black
 const COLOR_TEMP = Color.red
+const COLOR_NONE = Color.white
 
 func _ready():
 	pass
