61f44170d53c72e863d9f124ea2ec0cf16ccd898
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-03-31 13:15:01 -0600

    Function to update on text change

--- godot/hex_panel/HexRow.gd
+++ godot/hex_panel/HexRow.gd
@@ -10,8 +10,34 @@ func _ready():
 
 
 func set_line(text: String):
+	var length := text.length()
+	var i := 0
+	while i < length and i < get_child_count():
+		var c = get_child(i)
+		if not c is Label:
+			remove_child(c)
+			c.queue_free()
+		else:
+			c.text = text.substr(i, 1)
+			i += 1
+	
+	if i >= get_child_count():
+		# add extra
+		pass
+	
+	if i >= length:
+		# remove remaining children
+		pass
+	
 	print(text.length())
+#	print(get_child_count())
 
 
 func get_line() -> String:
 	return line_text
+
+
+func add_letter(letter: String) -> void:
+	var l := Label.new()
+	l.text = letter.substr(0, 1)
+	add_child(l)
