af0960a550b879ead7480ec0fb33339cf6d8dbcb
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-04-01 11:22:32 -0600

    Expanded HexContainer script

--- godot/hex_panel/HexContainer.gd
+++ godot/hex_panel/HexContainer.gd
@@ -1,5 +1,14 @@
 extends Container
 
+export var text: String setget set_text
 
 func _ready():
+	pass
+
+
+func split_lines(lines: String) -> PoolStringArray:
+	return $StringParser.parse(lines)
+
+
+func set_text(new_text: String):
 	pass
