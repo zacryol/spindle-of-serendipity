a07400e089c7d01509023630fbfc4916fe9c55f1
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-04-02 13:50:40 -0600

    Cleared broke functions

--- godot/hex_panel/StringParser.gd
+++ godot/hex_panel/StringParser.gd
@@ -29,27 +29,3 @@ func parse(input: String) -> PoolStringArray:
 	# Adjust indentation
 	# Something goes here
 	return lines
-
-
-func is_broke(lines: PoolStringArray) -> bool:
-	# Check if any gaps go from top to bottom
-	# For each space in the top row, check if either node below is also a space
-	#	Or beyond line end
-	#	recursively
-	# For even rows (0, 2, 4) and given space x check nodes in next row at positions x and x - 1
-	# For odds (1, 3, 5) check x and x + 1
-	
-	
-	return true
-
-
-func longest_line_in(lines: PoolStringArray) -> int:
-	if lines.size() == 1:
-		return 0
-	
-	var line_id := 0
-	for i in range(1, lines.size()):
-		if lines[i].length() > lines[line_id].length():
-			line_id = i
-	
-	return line_id
