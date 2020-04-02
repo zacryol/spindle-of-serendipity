e70605b430fb999d2731f78953528b07fc48eb25
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-04-02 13:31:48 -0600

    Wrote comments/pseudocode

--- godot/hex_panel/StringParser.gd
+++ godot/hex_panel/StringParser.gd
@@ -5,6 +5,8 @@ func parse(input: String) -> PoolStringArray:
 	var lines: PoolStringArray
 	var max_line_length := sqrt(2 * input.length())
 	var current_line: String = ""
+	
+	# Create lines
 	for s in input.split(" "):
 		if s.length() >= max_line_length:
 			if current_line:
@@ -20,6 +22,20 @@ func parse(input: String) -> PoolStringArray:
 			lines.append(current_line)
 			current_line = ""
 	
+	# Append leftover
 	if current_line:
 		lines.append(current_line)
+	
+	# Adjust indentation
+	# Something goes here
 	return lines
+
+
+func is_broke(lines: PoolStringArray) -> bool:
+	# Check if any gaps go from top to bottom
+	# For each space in the top row, check if either node below is also a space
+	#	Or beyond line end
+	#	recursively
+	# For even rows (0, 2, 4) and given space x check nodes in next row at positions x and x - 1
+	# For odds (1, 3, 5) check x and x + 1
+	return true
