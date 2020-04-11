5e0c2c2417a2d49512f7b8086fe770c49b3a69af
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-04-11 15:59:53 -0600

    Verify function if HexContainer

--- godot/hex_panel/HexContainer.gd
+++ godot/hex_panel/HexContainer.gd
@@ -62,7 +62,16 @@ func pop_solve() -> void:
 		if get_child(i).pop_solve():
 			return
 		i -= 1
-	pass
+
+
+func verify() -> bool:
+	for c in get_children():
+		if not c is HexRow:
+			continue
+		else:
+			if not c.verify():
+				return false
+	return true
 
 
 func reveal_all():
