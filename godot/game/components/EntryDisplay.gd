e97b76438b4f514bbec0612605e3ec2eeca80fbc
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-04-19 12:45:10 -0600

    Remove unused function

--- godot/game/components/EntryDisplay.gd
+++ godot/game/components/EntryDisplay.gd
@@ -54,16 +54,6 @@ func is_solved() -> bool:
 	return hex.verify()
 
 
-func get_letters_in_entry() -> PoolStringArray:
-	var letters: PoolStringArray = []
-	for i in entry_text.length():
-		var current_char := get_char_at(i)
-		if bool_mask.has(current_char):
-			if not current_char in letters:
-				letters.append(current_char)
-	return letters
-
-
 func get_char_at(index: int) -> String:
 	return entry_text.substr(index, 1)
 
