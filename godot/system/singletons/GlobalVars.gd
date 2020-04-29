422e665cdb9a01013c22468d66bd797c127c42f5
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-04-29 11:27:46 -0600

    Added enum, vars for win condition

--- godot/system/singletons/GlobalVars.gd
+++ godot/system/singletons/GlobalVars.gd
@@ -36,6 +36,17 @@ var p1_name_default := "Player 1"
 var p2_name_default := "Player 2"
 var p3_name_default := "Player 3"
 
+# Game Type, condition
+enum Type{
+	FREE,
+	ROUNDS,
+	SCORE,
+}
+
+var win_score := 1000
+var rounds := 7
+# End Type
+
 func _ready():
 	load_settings_from_file()
 	
