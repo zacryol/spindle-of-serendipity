b731625196ed126e18184a5f90bc68bc4faaf57b
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-06-14 10:57:46 -0600

    added new round function to round sign

--- godot/game/RoundSign.gd
+++ godot/game/RoundSign.gd
@@ -3,5 +3,14 @@ extends CanvasLayer
 var message := "Round %s"
 var suffix := " of %s"
 
+onready var label: Label = $Main/PanelContainer/Label
+
 func _ready():
 	pass
+
+
+func new_round(num: int) -> void:
+	var show := message % str(num)
+	if GlobalVars.game_type == GlobalVars.Type.ROUNDS:
+		show += suffix % str(num)
+	label.text = show
