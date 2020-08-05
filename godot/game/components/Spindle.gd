508a4c6dbc6608f309730c07c6a22a23a2de70cc
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-08-05 17:54:07 -0600

    Spindle uses states instead of bool

--- godot/game/components/Spindle.gd
+++ godot/game/components/Spindle.gd
@@ -8,32 +8,51 @@ enum State {
 	ACTIVE, # Enabled. Player needs to click
 	RUNNING, # Spinning. Click to set score
 }
+
+var current_state: int = State.INACTIVE
+
 var current_value: int
 var enabled := false setget set_enabled
 
 func set_enabled(enab: bool) -> void:
 	enabled = enab
 	$Button.disabled = not enabled
+	print("SHOULD NOT HAPPEN")
+
+
+func set_state(new_state: int) -> void:
+	current_state = new_state
+	match new_state:
+		State.INACTIVE:
+			$Button.disabled = true
+		State.ACTIVE:
+			$Button.disabled = false
 
 
 func _letters_guessed(number: int, solves: bool):
-	self.enabled = false
+	#self.enabled = false
+	set_state(State.INACTIVE)
 	emit_signal("score", number * current_value, solves)
 
 
 func _on_Button_pressed():
-	if enabled:
+	#if enabled:
+	#	pass
+	if current_state == State.ACTIVE:
 		emit_signal("game_log", "")
 		current_value = (randi() % 10 + 1) * 5
 		$ScoringLabel.text = "Score: " + str(current_value)
-		self.enabled = false
+		#self.enabled = false
+		set_state(State.INACTIVE)
 		emit_signal("game_log", str(current_value) + " points per letter")
 		emit_signal("spun")
 
 
 func _on_PlayersPanel_turn_done():
-	self.enabled = true
+	#self.enabled = true
+	set_state(State.ACTIVE)
 
 
 func _on_EntryDisplay_text_ready():
-	self.enabled = true
+	#self.enabled = true
+	set_state(State.ACTIVE)
