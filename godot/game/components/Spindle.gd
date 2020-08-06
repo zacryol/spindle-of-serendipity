6d85f866680ce5e7e7acfab7d2e69f87e7c66dd1
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-08-05 18:00:57 -0600

    Removed old bool

--- godot/game/components/Spindle.gd
+++ godot/game/components/Spindle.gd
@@ -10,15 +10,7 @@ enum State {
 }
 
 var current_state: int = State.INACTIVE
-
 var current_value: int
-var enabled := false setget set_enabled
-
-func set_enabled(enab: bool) -> void:
-	enabled = enab
-	$Button.disabled = not enabled
-	print("SHOULD NOT HAPPEN")
-
 
 func set_state(new_state: int) -> void:
 	current_state = new_state
@@ -30,29 +22,23 @@ func set_state(new_state: int) -> void:
 
 
 func _letters_guessed(number: int, solves: bool):
-	#self.enabled = false
 	set_state(State.INACTIVE)
 	emit_signal("score", number * current_value, solves)
 
 
 func _on_Button_pressed():
-	#if enabled:
-	#	pass
 	if current_state == State.ACTIVE:
 		emit_signal("game_log", "")
 		current_value = (randi() % 10 + 1) * 5
 		$ScoringLabel.text = "Score: " + str(current_value)
-		#self.enabled = false
 		set_state(State.INACTIVE)
 		emit_signal("game_log", str(current_value) + " points per letter")
 		emit_signal("spun")
 
 
 func _on_PlayersPanel_turn_done():
-	#self.enabled = true
 	set_state(State.ACTIVE)
 
 
 func _on_EntryDisplay_text_ready():
-	#self.enabled = true
 	set_state(State.ACTIVE)
