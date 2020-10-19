717278137102f450b46e18c3670d5dffc2b78089
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-10-19 12:25:28 -0600

    Button to view folder

--- godot/game/menus/EntryStudio.gd
+++ godot/game/menus/EntryStudio.gd
@@ -2,5 +2,8 @@ extends Control
 
 
 func _ready():
-	#OS.shell_open(OS.get_user_data_dir() + "/entries")
 	pass
+
+
+func _on_FolderButton_pressed():
+	OS.shell_open(OS.get_user_data_dir() + "/entries")
