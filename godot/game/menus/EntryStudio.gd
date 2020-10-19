c9dc7a430ac0d0c16e413adb96bd288fdba00c06
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-10-19 13:06:25 -0600

    Items can be added to menu

--- godot/game/menus/EntryStudio.gd
+++ godot/game/menus/EntryStudio.gd
@@ -2,9 +2,22 @@ extends Control
 
 var single := preload("res://game/menus/ESSingle.tscn")
 
+onready var vbox: VBoxContainer = $PanelContainer/HSplitContainer/ScrollContainer/VBoxContainer
+onready var add_button: Button = $PanelContainer/HSplitContainer/ScrollContainer/VBoxContainer/AddButton
+
 func _ready():
-	pass
+	add_item()
+
+
+func add_item():
+	var s := single.instance()
+	vbox.add_child(s)
+	vbox.move_child(add_button, vbox.get_child_count() - 1)
 
 
 func _on_FolderButton_pressed():
 	OS.shell_open(OS.get_user_data_dir() + "/entries")
+
+
+func _on_AddButton_pressed():
+	add_item()
