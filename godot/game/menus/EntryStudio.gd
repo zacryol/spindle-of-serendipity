48f6290518e297bae0f41edff697c36a7099a169
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-10-23 12:40:14 -0600

    Set return types

--- godot/game/menus/EntryStudio.gd
+++ godot/game/menus/EntryStudio.gd
@@ -5,19 +5,19 @@ var single := preload("res://game/menus/ESSingle.tscn")
 onready var vbox := $PC/HSplit/PContainer/Scroll/VBox as VBoxContainer
 onready var add_button := $PC/HSplit/PContainer/Scroll/VBox/AddButton as Button
 
-func _ready():
+func _ready() -> void:
 	add_item()
 
 
-func add_item():
+func add_item() -> void:
 	var s := single.instance()
 	vbox.add_child(s)
 	vbox.move_child(add_button, vbox.get_child_count() - 1)
 
 
-func _on_FolderButton_pressed():
+func _on_FolderButton_pressed() -> void:
 	OS.shell_open(OS.get_user_data_dir() + "/entries")
 
 
-func _on_AddButton_pressed():
+func _on_AddButton_pressed() -> void:
 	add_item()
