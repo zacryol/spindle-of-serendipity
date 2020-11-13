14aabee1b3edefda1fd8b9202f9e217c35506334
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2020-11-13 15:46:42 -0700

    added delete button

--- godot/game/menus/ESSingle.gd
+++ godot/game/menus/ESSingle.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 onready var entry_enter := $HBoxContainer/E as LineEdit
 onready var cat_enter := $HBoxContainer/C as LineEdit
 onready var sou_enter := $HBoxContainer/S as LineEdit
+onready var try_delete := $HBoxContainer/DeleteButton as Button
 
 func _ready() -> void:
 	pass
@@ -24,3 +25,7 @@ func set_values(e := "", c := "", s := "") -> void:
 	entry_enter.text = e
 	cat_enter.text = c
 	sou_enter.text = s
+
+
+func _on_DeleteButton_pressed() -> void:
+	pass
