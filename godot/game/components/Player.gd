1a706a92611e7c6f9e53701a416f40440659fa58
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2021-01-04 12:18:38 -0700

    remove deprecated code/nodes

--- godot/game/components/Player.gd
+++ godot/game/components/Player.gd
@@ -6,10 +6,6 @@ var player_name: String
 var template := "%s: %d (%d)"
 var template2 := "%s: %d (%d / %d)"
 
-onready var name_label := $PanelContainer/CenterContainer/VBoxContainer/PlayerLabel
-onready var score_label := $PanelContainer/CenterContainer/VBoxContainer/ScoreLabelCurr
-onready var total_label := $PanelContainer/CenterContainer/VBoxContainer/TotalLabel
-
 onready var info := $PanelContainer/CenterContainer/InfoLabel as Label
 
 func set_label_text() -> void:
@@ -21,7 +17,6 @@ func set_label_text() -> void:
 
 func add_to_score(points: int) -> void:
 	score += points
-#	score_label.text = "Score: " + str(score)
 	set_label_text()
 	
 	if GlobalVars.win_by_score():
@@ -31,17 +26,12 @@ func add_to_score(points: int) -> void:
 
 func set_name(new_name: String) -> void:
 	player_name = new_name
-#	name_label.text = player_name
 	set_label_text()
 
 
 func cache_score() -> void:
 	total += score
 	add_to_score(-score)
-#	total_label.text = "Total: " + str(total)
-#
-#	if GlobalVars.win_by_score():
-#		total_label.text += " / " + str(GlobalVars.win_score)
 
 
 func get_all_score() -> int:
