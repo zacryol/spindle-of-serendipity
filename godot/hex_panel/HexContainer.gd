cb2e46e265b42f596901ccaccf1e3ac13995d4d5
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2021-05-31 12:51:18 -0600

    move line separation into main HexContainer script

--- godot/hex_panel/HexContainer.gd
+++ godot/hex_panel/HexContainer.gd
@@ -44,8 +44,37 @@ func _notification(what):
 			count += 1
 
 
-func split_lines(lines: String) -> PoolStringArray:
-	return $StringParser.parse(lines)
+func split_lines(input: String) -> PoolStringArray:
+	var lines := PoolStringArray()
+	var max_line_length: int = int(sqrt(2 * input.length()))
+	var current_line: String = ""
+	
+	# Create lines
+	for s in input.split(" "):
+		if not s:
+			continue
+		
+		if s.length() >= max_line_length:
+			if current_line:
+				lines.append(current_line)
+				current_line = ""
+		
+		if current_line:
+			current_line = current_line + " " + s
+		else:
+			current_line = s
+		
+		if current_line.length() >= max_line_length:
+			lines.append(current_line)
+			current_line = ""
+	
+	# Append leftover
+	if current_line:
+		lines.append(current_line)
+	
+	# Adjust indentation
+	# Something goes here
+	return lines
 
 
 func set_text(new_text: String):
