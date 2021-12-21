5a198019b1b09fb9a1820c90e12a0f4063c78071
zacryol <5291432-zacryol@users.noreply.gitlab.com>
2021-12-21 15:46:02 -0700

    Change deployment configs
    Since the docker image for CD does not (currently) use rcedit for Windows export,
    and the game should be in a more "professional" state.

--- deploy-game.sh
+++ deploy-game.sh
@@ -7,13 +7,13 @@ rm export/mac/*
 echo Old builds removed . . . 
 
 echo Exporting Linux build . . . 
-godot-3.2.1-headless --path godot --export "Linux/X11" ../export/lin/spindle-of-serendipity.x86_64
+godot-3.2.3 --path godot --export "Linux/X11" ../export/lin/spindle-of-serendipity.x86_64
 
 echo Exporting Windows build
-godot-3.2.1-headless --path godot --export "Windows Desktop" ../export/win/spindle-of-serendipity.exe
+godot-3.2.3 --path godot --export "Windows Desktop" ../export/win/spindle-of-serendipity.exe
 
 echo Exporting Mac build
-godot-3.2.1-headless --path godot --export "Mac OSX" ../export/mac/spindle-of-serendipity-mac.zip
+godot-3.2.3 --path godot --export "Mac OSX" ../export/mac/spindle-of-serendipity-mac.zip
 
 echo Pushing to Itch.io via butler . . .
 butler push export/lin zacryol/spindle-of-serendipity:linux --userversion-file version.txt
